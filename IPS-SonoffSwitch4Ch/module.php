a1d65b101181404dce27b43584ac97bf7258a20b
micbuh <micbuh@gmail.com>
2017-12-17 01:00:10 +0100

    fully implemented IPS-SonoffSwitch4Ch (Sonoff 4CH)

--- IPS-SonoffSwitch4Ch/module.php
+++ IPS-SonoffSwitch4Ch/module.php
@@ -7,12 +7,15 @@ class IPS_SonoffSwitch4Ch extends IPSModule {
       $this->ConnectParent("{EE0D345A-CF31-428A-A613-33CE98E752DD}");
       //Anzahl die in der Konfirgurationsform angezeigt wird - Hier Standard auf 1
       $this->RegisterPropertyString("Topic","");
-      $this->RegisterPropertyString("On","1");
-      $this->RegisterPropertyString("Off","0");
+      $this->RegisterPropertyString("On","ON");
+      $this->RegisterPropertyString("Off","OFF");
       $this->RegisterPropertyString("FullTopic","%prefix%/%topic%");
 
-      $variablenID = $this->RegisterVariableBoolean("SonoffStatus", "Status","~Switch");
-      $this->EnableAction("SonoffStatus");
+      for($n = 1;$n<5;$n++)
+      {
+        $this->RegisterVariableBoolean("SonoffStatus_Ch$n", "Status_Ch$n","~Switch");
+        $this->EnableAction("SonoffStatus_Ch$n");
+      }
 
   }
   public function ApplyChanges() {
@@ -22,69 +25,71 @@ class IPS_SonoffSwitch4Ch extends IPSModule {
       //Setze Filter fÃ¼r ReceiveData
       $topic = $this->ReadPropertyString("Topic");
       $this->SetReceiveDataFilter(".*".$topic.".*");
-    }
+  }
 
-    public function ReceiveData($JSONString) {
-      $this->SendDebug("JSON", $JSONString,0);
-      $data = json_decode($JSONString);
-      $off = $this->ReadPropertyString("Off");
-      $on = $this->ReadPropertyString("On");
+  public function ReceiveData($JSONString) {
+    $this->SendDebug("JSON", $JSONString,0);
+    $data = json_decode($JSONString);
+    $off = $this->ReadPropertyString("Off");
+    $on = $this->ReadPropertyString("On");
 
-      // Buffer decodieren und in eine Variable schreiben
-      $Buffer = utf8_decode($data->Buffer);
-      // Und Diese dann wieder dekodieren
-      IPS_LogMessage("SonoffSwitch",$data->Buffer);
+    // Buffer decodieren und in eine Variable schreiben
+    $Buffer = utf8_decode($data->Buffer);
+    // Und Diese dann wieder dekodieren
+    IPS_LogMessage("SonoffSwitch",$data->Buffer);
 	  $Buffer = json_decode($data->Buffer);
-	  if (fnmatch("*POWER", $Buffer->TOPIC)) {
+
+	  if (fnmatch("*POWER[1234]", $Buffer->TOPIC)) {
 		  $this->SendDebug("Power", $Buffer->MSG,0);
       switch ($Buffer->MSG) {
         case $off:
-          SetValue($this->GetIDForIdent("SonoffStatus"), 0);
+          SetValue($this->GetIDForIdent("SonoffStatus_Ch" .substr(" $Buffer->TOPIC", -1)), 0);
           break;
         case $on:
-          SetValue($this->GetIDForIdent("SonoffStatus"), 1);
+          SetValue($this->GetIDForIdent("SonoffStatus_Ch" .substr(" $Buffer->TOPIC", -1)), 1);
           break;
       }
 	  }
-      $this->SendDebug("Buffer", $Buffer->TOPIC,0);
+    $this->SendDebug("Buffer", $Buffer->TOPIC,0);
+  }
+
+  public function setState($n /*channel*/, $Value) {
+    SetValue($this->GetIDForIdent("SonoffStatus_Ch$n"), $Value);
+
+
+    $FullTopic = explode("/",$this->ReadPropertyString("FullTopic"));
+    $PrefixIndex = array_search("%prefix%",$FullTopic);
+    $TopicIndex = array_search("%topic%",$FullTopic);
+
+    $SetCommandArr = $FullTopic;
+    $index = count($SetCommandArr);
+
+    $SetCommandArr[$PrefixIndex] = "cmnd";
+    $SetCommandArr[$TopicIndex] = $this->ReadPropertyString("Topic");
+    $SetCommandArr[$index] = "power$n";
+
+    $topic = implode("/",$SetCommandArr);
+  	$msg = $Value;
+
+  	if($msg===false){$msg = 'false';}
+  	elseif($msg===true){$msg = 'true';}
+
+  	$Buffer["Topic"] = $topic;
+  	$Buffer["MSG"] = $msg;
+  	$BufferJSON = json_encode($Buffer);
+  	//MQTT_Publish(33877 /*[MQTT Client]*/, $topic,$msg,0,0);
+  	$this->SendDebug("setStatus", $BufferJSON,0);
+    $this->SendDataToParent(json_encode(Array("DataID" => "{018EF6B5-AB94-40C6-AA53-46943E824ACF}", "Action" => "Publish", "Buffer" => $BufferJSON)));
+  }
+
+  public function RequestAction($Ident, $Value) {
+    switch ($Ident) {
+      case fnmatch("SonoffStatus_Ch[1234]", $Ident):
+        $result = $this->setState(substr($Ident, -1), $Value);
+        break;
+      default:
+        throw new Exception("Invalid ident");
     }
-
-  public function setStatus($Value) {
-  SetValue($this->GetIDForIdent("SonoffStatus"), $Value);
-
-  $FullTopic = explode("/",$this->ReadPropertyString("FullTopic"));
-  $PrefixIndex = array_search("%prefix%",$FullTopic);
-  $TopicIndex = array_search("%topic%",$FullTopic);
-
-  $SetCommandArr = $FullTopic;
-  $index = count($SetCommandArr);
-
-  $SetCommandArr[$PrefixIndex] = "cmnd";
-  $SetCommandArr[$TopicIndex] = $this->ReadPropertyString("Topic");
-  $SetCommandArr[$index] = "power";
-
-  $topic = implode("/",$SetCommandArr);
-	$msg = $Value;
-
-	if($msg===false){$msg = 'false';}
-	elseif($msg===true){$msg = 'true';}
-
-	$Buffer["Topic"] = $topic;
-	$Buffer["MSG"] = $msg;
-	$BufferJSON = json_encode($Buffer);
-	//MQTT_Publish(33877 /*[MQTT Client]*/, $topic,$msg,0,0);
-	$this->SendDebug("setStatus", $BufferJSON,0);
-  $this->SendDataToParent(json_encode(Array("DataID" => "{018EF6B5-AB94-40C6-AA53-46943E824ACF}", "Action" => "Publish", "Buffer" => $BufferJSON)));
-}
-
-    public function RequestAction($Ident, $Value) {
-      switch ($Ident) {
-        case "SonoffStatus":
-          $result = $this->setStatus($Value);
-          break;
-        default:
-          throw new Exception("Invalid ident");
-      }
-    }
+  }
 }
 ?>
